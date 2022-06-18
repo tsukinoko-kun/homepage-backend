ed6e1da967039a0b240b4657e3aadf16ad49da7c
Frank Mayer <frank.mayer1907@protonmail.com>
2022-06-18 14:54:51 +0200

    fixed request

--- homepage-backend/Controllers/message.fs
+++ homepage-backend/Controllers/message.fs
@@ -1,7 +1,7 @@
 namespace homepage_backend
 
 open System
-open System.IO
+open System.Text;
 open System.Net.Http
 open System.Net.Http.Json
 
@@ -12,28 +12,23 @@ module message =
         | null -> failwith $"Environment variable '{key}' not set"
         | value -> value
 
-    type Post (from: string, text: string) =
-        let from = from
-        let ``to`` = "mail@frank-mayer.io"
-        let subject = "New message from homepage"
-        let text = text
+    let apiKey = env "MAILGUN_API_KEY"
+    let domainName = env "MAILGUN_DOMAIN_NAME"
 
     let send (mail: string, message: string) =
+        use httpClient = new HttpClient()
+        use request = new HttpRequestMessage(new HttpMethod("POST"), $"https://api.mailgun.net/v3/{domainName}/messages")
+        let mutable base64authorization = Convert.ToBase64String (Encoding.ASCII.GetBytes ($"api:{apiKey}"))
+        request.Headers.TryAddWithoutValidation ("Authorization", $"Basic {base64authorization}") |> ignore
+        let mutable multipartContent = new MultipartFormDataContent()
+        multipartContent.Add (new StringContent(mail), "from")
+        multipartContent.Add (new StringContent("mail@frank-mayer.io"), "to")
+        multipartContent.Add (new StringContent("New message from homepage"), "subject")
+        multipartContent.Add (new StringContent(message), "text")
+        request.Content <- multipartContent
         task {
-        let apiKey = env "MAILGUN_API_KEY"
-        let domainName = env "MAILGUN_DOMAIN_NAME"
-
-        // new Post
-        let post = new Post(mail, message)
-
-        use client = new HttpClient()
-        let byteArray = UTF8.bytes $"api:{apiKey}"
-        let b64EAuthString = Convert.ToBase64String byteArray
-        client.DefaultRequestHeaders.Authorization = new Headers.AuthenticationHeaderValue("Basic", b64EAuthString)
-            |> ignore
-            
-        let! resp = client.PostAsJsonAsync($"https://api.mailgun.net/v3/{domainName}/messages", post)
-        return resp.Content.ReadAsStringAsync()
+            let! resp = httpClient.SendAsync request
+            return resp.Content.ReadAsStringAsync()
         }
         |> Async.AwaitTask
         |> Async.RunSynchronously
