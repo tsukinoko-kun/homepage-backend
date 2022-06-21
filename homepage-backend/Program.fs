941fcd9c51615b3869702c09d1a9a40b75845c19
Frank Mayer <frank.mayer1907@protonmail.com>
2022-06-21 18:05:00 +0200

    .env

--- homepage-backend/Program.fs
+++ homepage-backend/Program.fs
@@ -3,12 +3,20 @@ namespace homepage_backend
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
+open dotenv.net
 
 module Program =
     let exitCode = 0
+        
 
     [<EntryPoint>]
     let main args =
+        DotEnv
+            .Fluent()
+            .WithoutExceptions()
+            .WithTrimValues()
+            .WithOverwriteExistingVars()
+            .Load()
 
         let builder = WebApplication.CreateBuilder(args)
 
